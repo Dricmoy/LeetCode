21274751ade089cb1bb5d2fa93b0e7981cb6a54a
Dricmoy Bhattacharjee <dricbhattacharjee@gmail.com>
2024-05-08 22:45:05 -0600

    first commit

---
+++ Easy-1464.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if (nums.size() == 2){
+            return (nums[0]-1) * (nums[1]-1);
+        }
+
+        int j = *max_element(nums.begin(), nums.end());
+
+        auto iter = std::find(nums.begin(), nums.end(), j);
+
+        if (iter != nums.end()){
+            nums.erase(iter);
+        }
+
+        int i = *max_element(nums.begin(), nums.end());
+        return (i-1) * (j-1);
+    }
+};
