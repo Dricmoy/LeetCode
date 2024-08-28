0d30fdb75e500724bf854245843ce2e4187e307a
Dricmoy Bhattacharjee <dricbhattacharjee@gmail.com>
2024-08-28 12:13:52 -0600

    vertical scanning sol, space complexity O(1) time complexity:O(n * m)/O(S) S= all strings+chars

--- Easies/14.longest-common-prefix.py
+++ Easies/14.longest-common-prefix.py
@@ -6,11 +6,21 @@
 
 # @lc code=start
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        common = ''
+    def longestCommonPrefix(self, strs: List[str]) -> str: # type: ignore
+        if not strs: #easiest way  to check for empty
+            return ''
         
-        for words in strs:
+        prefix = strs[0]
+        
+        for str in strs[1:]:
+            while not str.startswith(prefix):
+                prefix = prefix[:-1] #this removes the last character with slicing, same as [0:-1]
                 
+                if not prefix: #if prefix is empty by the last scan, this means none exists
+                    return ''
+                
+        return prefix
+                        
             
 # @lc code=end
 
